e17267b847434657839175667b12d28e98cb1c9c
thedeuce2 <loxcane@yahoo.com>
2026-04-09 08:25:09 -0400

    Add calendar data to data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+const calendarData = [
+    { date: '2026-04-01', event: 'Team Meeting' },
+    { date: '2026-04-08', event: 'Game Day' },
+    { date: '2026-04-15', event: 'Practice Session' },
+    { date: '2026-04-22', event: 'Tournament' }
+];
+
+export default calendarData;
